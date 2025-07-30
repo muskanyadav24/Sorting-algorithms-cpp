07f593849996916e25e6a422462c4d5a054c62d8
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-30 08:57:32 +0530

    bubble-sort-using-vector.cpp

--- bubble-sort-using-vector.cpp
+++ bubble-sort-using-vector.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
 
+  int n;
+
+  cout << "Enter the size of array :- ";
+  cin >> n;
+
+  vector<int> array(n);
+
+  for(int i=0;i<n;i++){
+    cout << "Array of [" << i << "] :- " ;
+    cin >> array[i];
+  }
+
+  for(int i=0;i<=n-1;i++){
+    for(int j=0;j<=n-2;j++){
+      if(array[j] > array[j+1]){
+        swap(array[j],array[j+1]);
+      }
+    }
+  }
+
+  cout << "Sorted array :- ";
+  
+  for(int k=0;k<n;k++){
+    cout << array[k] << " , ";
+  }
+
+  cout << endl;
+  
+  return 0;
+}
