e18d58654593f17bbb168fdf6f2ad7296ccc58c7
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-31 08:33:32 +0530

    selection-sort-static

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+void selectionSort(int uns[], int n){
+
+    for(int i=0;i<n;i++){
+        int minInd = i;
+        // int j = i+1;
+
+        for(int j=i+1;j<n;j++){
+            if(uns[j] < uns[minInd]){
+                minInd = j;
+            }
+        }
+        swap(uns[minInd], uns[i]);
+    }
+}
+
+void printSort(int uns[],int n){
+
+    for(int i=0;i<n;i++){
+        cout << uns[i] << " , ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+    int arr[5] = {67,87,2,4,21};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Unsorted Array :- ";
+    printSort(arr,n);
+
+    selectionSort(arr, n);
+
+    cout << "Sorted Array :- ";
+    printSort(arr,n);
+
+    return 0;
+}
